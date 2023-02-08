860f5c63e2de2485d1e0884866c5b9ae42ae606a
slil_puangpoom <slil.pua@MacBook-Air-khxng-slil-puangpoom.local>
2023-02-08 21:05:29 +0700

    fix : cover image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/templates/Layout";
 import Head from "next/head";
-import coverImage from "../public/web-cover.png";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,10 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>Blink Me Code</title>
           <link rel="blink icon" href="/icon/btc.png" />
-          <meta
-            property="og:image"
-            content={`https://www.blinkmecode.com${coverImage}`}
-          />
+          <meta property="og:image" content="/web-cover.png" />
         </Head>
         <Component {...pageProps} />
       </Layout>
